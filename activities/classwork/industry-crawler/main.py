9e6e57b031df0d72000ea7f85722682f786c0400
pau <pau.rodriguez1695@gmail.com>
2019-12-01 14:41:41 -0600

    crawler

--- activities/classwork/industry-crawler/main.py
+++ activities/classwork/industry-crawler/main.py
@@ -12,18 +12,37 @@ DEFAULT_INDUSTRY_FILE = "industries.json"
 logger = logging.getLogger(__name__)
 
 
+
 class Main(object):
+
+
+    def _recursive_search(self, node, string_wrapper, exact):
+         title = node["title"]
+        children = node["children"]
+        new_children = []
+        for child in children:
+            child_valid = self._recursive_search(child, string_wrapper, exact=exact)
+            if child_valid:
+                new_children.append(child)
+        node["children"] = new_children
+        result = len(new_children) or string_wrapper.boolean_search(title, reverse=True, exact=exact)
+        return result
+
 
     @staticmethod
     @timeit(logger)
-    def download(filename=DEFAULT_INDUSTRY_FILE):
-        pass
 
-    @timeit(logger)
     def search(self, title, exact=False, filename=DEFAULT_INDUSTRY_FILE):
-        pass
+        target_title = StringWrapper(value=title)
+        sic_industries = SIC.load_json(filename)
+        return []  # return list of the matching industries
+        new_children = []
+        for child in children:
+            if self._recursive_search(child, target_title, exact=exact):
+                new_children.append(child)
+        return new_children
 
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    fire.Fire(Main)
+
+
+if __name__ == "__main__":	if __name__ == "__main__":
--- activities/classwork/industry-crawler/model.py
+++ activities/classwork/industry-crawler/model.py
@@ -1,10 +1,74 @@
-import logging
-
-import json
-import requests
-
-from bs4 import BeautifulSoup
+class AbstractIndustry(object):
 
 
-class SIC:
-    pass
+    def __init__(self, title, children):
+        logger.info("Creating Industry_ {title}".format(title=title))
+        self.title = title
+        self.children = children
+
+
+
+    def level(self):
+        raise NotImplementedError("Abstract industry does not contains level.")
+
+
+    def add_child(self, child):
+        self.children.append(child)
+
+
+    def to_dict(self):
+        return{
+            "title": self.title,
+            "children": [child.to_dict() for child in self.children]
+        }
+
+
+    def jsonify(self):
+    def from_url(url):
+        response = requests.get(url)
+        html = BeautifulSoup(response.text, 'html.parser')
+        return MajorGroup(
+            title=[elm.text for elm in html.find_all("h2") if elm.text.lower().startswith("major group")][0],	            title=[
+
+            children=[
+                Group(
+                    title=group.text,
+                    children=[
+                        Single(
+                            title=f"{inner.parent.text}",
+                            children=[]
+                        )	                        )
+                        for inner in html.find_all("a")
+                        if inner.attrs.get("href", "").startswith("sic_manual")
+                        and inner.parent.text.startswith(group.text.split(":")[0].split(" ")[-1])
+                    ]	                    ]
+                )	                )
+                for group in html.find_all("strong") if group.text.lower().startswith("industry group")
+            ]	            ]
+        )	        )
+
+
+
+class Group(AbstractIndustry):
+    level = "SIC Group"
+
+
+class SIC(AbstractIndustry):
+    @staticmethod
+    def load_json(filename):
+        with open(filename, "r") as file:
+            sic_industries = json.load(file.read())
+        return sic_industries
+
+
+    @staticmethod
+    def from_url(url):
+
+            elif href.endswith("group"):
+                major_group_url = url.replace("sic_manual.html", href)
+                divisions[-1].add_child(MajorGroup.from_url(major_group_url))
+        return Single(
+            title="SIC",
+            children=divisions
+        )
+
