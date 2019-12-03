4ecea5d1730f05db68b6e1e99e1ae7eddcf0e8a4
pau <pau.rodriguez1695@gmail.com>
2019-12-02 20:20:55 -0600

    scala-intro

--- activities/classwork/scala-intro/src/main/scala/example/c/Main.scala
+++ activities/classwork/scala-intro/src/main/scala/example/c/Main.scala
@@ -4,7 +4,7 @@ object Main {
   val numbers: list[Int] = List(1,2,3,4,5)
 
   def procedure: Box[Int] =
-    number.map(num=> Box(num)).foldRight(Box(0)) {
+    numbers.map(num=> Box(num)).foldRight(Box(0)) {
       case (elm, z) => z.concate(elm).map(_.toInt)
     }
   def main(args: Array[String]): Unit= args match{
